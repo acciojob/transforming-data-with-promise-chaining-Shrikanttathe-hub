e749295ac3300fc06df3d3d8f7b9a99dde9af775
Shrikanttathe-hub <142688445+Shrikanttathe-hub@users.noreply.github.com>
2024-02-24 22:40:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+
+ const btn = document.getElementById('btn');
+
+btn.addEventListner('click', function(){
+	const inputElement = document.getElementById('ip');
+	cosnt inputValue = inputElement.value;
+   alert("Please enter valid valie:" + inputValue);
+});
+
+function getNumber(Number){
+	return new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			const result = ip.filter((num)=>num > 0 || num != NAN);
+			output.innerText = result.join("Result:, ");
+			resolve(result);
+			
+		}, 2000);
+	});
+}
+
+getNumber();
